56645e21a5c08ce0b3177512a0304b21bbeb2416
DominikK03 <dominikkepczyk@o2.pl>
2024-08-04 16:51:36 +0200

    Repaired some bugs

--- src/Responeses/RedirectResponse.php
+++ src/Responeses/RedirectResponse.php
@@ -9,6 +9,7 @@ class RedirectResponse extends Response
 public function __construct(string $url, array $params)
 {
     parent::__construct('',ContentType::text);
+    header("Location: ". $url . "?" . http_build_query($params));
 
 }
 
